db0f833070acd99419c4edaa615f4ad2140a6b08
ahmedss1997 <asami6596@gmail.com>
2022-12-04 23:19:42 +0200

    doneee

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,6 @@ module.exports = {
     port: 8080,
     public: "0.0.0.0:8080",
   },
-  /*publicPath: "/Bayanatech/dist",*/
-  publicPath: "/",
+  publicPath: "/Bayanatech/dist",
+  // publicPath: "/",
 };
